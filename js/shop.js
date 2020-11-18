bb74cb4ee362f104cca0a558e06cc4089ec84074
digitsmart <69549818+digitsmart@users.noreply.github.com>
2020-11-18 00:02:59 -0800

    Add files via upload

--- js/shop.js
+++ js/shop.js
@@ -1,30 +1,60 @@
+function myFunction() {
+var x = document.getElementById("myDIV");
+var y = document.getElementById("imgs");
+if (x.style.display === "none") {
+  x.style.display = "block";
+  y.style.display = "none";
+} else {
+  x.style.display = "block";
+  y.style.display = "none";
+}
+}
+
+function myClose() {
+  var x = document.getElementById("myDIV");
+  var y = document.getElementById("imgs");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    y.style.display = "block";
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+  }
+
 function aa1() {
     var a = document.getElementById("aa");
     var b = document.getElementById("bb");
     var c = document.getElementById("cc");
     var d = document.getElementById("dd");
+    var e = document.getElementById("ee");
     var pa = document.getElementById("point1");
     var pb = document.getElementById("point2");
     var pc = document.getElementById("point3");
     var pd = document.getElementById("point4");
+    var pe = document.getElementById("point5");
     if (a.style.display === "none") {
       a.style.display = "block";
       b.style.display = "none";
       c.style.display = "none";
       d.style.display = "none";
+      e.style.display = "none";
       pa.style.display = "block";
       pb.style.display = "none";
       pd.style.display = "none";
       pc.style.display = "none";
+      pe.style.display = "none";
     } else {
       a.style.display = "block";
       b.style.display = "none";
       c.style.display = "none";
       d.style.display = "none";
+      e.style.display = "none";
       pa.style.display = "block";
       pb.style.display = "none";
       pd.style.display = "none";
       pc.style.display = "none";
+      pe.style.display = "none";
 
     }
   } 
@@ -34,28 +64,34 @@ function aa1() {
     var b = document.getElementById("bb");
     var c = document.getElementById("cc");
     var d = document.getElementById("dd");
+    var e = document.getElementById("ee");
     var pa = document.getElementById("point1");
     var pb = document.getElementById("point2");
     var pc = document.getElementById("point3");
     var pd = document.getElementById("point4");
+    var pe = document.getElementById("point4");
     if (b.style.display === "none") {
       b.style.display = "block";
       a.style.display = "none";
       c.style.display = "none";
       d.style.display = "none";
+      e.style.display = "none";
       pa.style.display = "none";
       pb.style.display = "block";
       pd.style.display = "none";
       pc.style.display = "none";
+      pe.style.display = "none";
     } else {
       b.style.display = "block";
       a.style.display = "none";
       c.style.display = "none";
       d.style.display = "none";
+      e.style.display = "none";
       pa.style.display = "none";
       pb.style.display = "block";
       pd.style.display = "none";
       pc.style.display = "none";
+      pe.style.display = "none";
     }
   } 
 
@@ -64,28 +100,34 @@ function aa1() {
     var b = document.getElementById("bb");
     var c = document.getElementById("cc");
     var d = document.getElementById("dd");
+    var e = document.getElementById("ee");
     var pa = document.getElementById("point1");
     var pb = document.getElementById("point2");
     var pc = document.getElementById("point3");
     var pd = document.getElementById("point4");
+    var pe = document.getElementById("point5");
     if (c.style.display === "none") {
       c.style.display = "block";
       a.style.display = "none";
       b.style.display = "none";
       d.style.display = "none";
+      e.style.display = "none";
       pa.style.display = "none";
       pb.style.display = "none";
       pd.style.display = "none";
       pc.style.display = "block";
+      pe.style.display = "none";
     } else {
       c.style.display = "block";
       a.style.display = "none";
       b.style.display = "none";
       d.style.display = "none";
+      e.style.display = "none";
       pa.style.display = "none";
       pb.style.display = "none";
       pd.style.display = "none";
       pc.style.display = "block";
+      pe.style.display = "none";
     }
   }
 
@@ -94,27 +136,69 @@ function aa1() {
     var b = document.getElementById("bb");
     var c = document.getElementById("cc");
     var d = document.getElementById("dd");
+    var e = document.getElementById("ee");
     var pa = document.getElementById("point1");
     var pb = document.getElementById("point2");
     var pc = document.getElementById("point3");
     var pd = document.getElementById("point4");
+    var pe = document.getElementById("point5");
     if (d.style.display === "none") {
       d.style.display = "block";
       a.style.display = "none";
       b.style.display = "none";
       c.style.display = "none";
+      e.style.display = "none";
       pa.style.display = "none";
       pd.style.display = "block";
       pb.style.display = "none";
       pc.style.display = "none";
+      pe.style.display = "none";
     } else {
       d.style.display = "block";
       a.style.display = "none";
       b.style.display = "none";
       c.style.display = "none";
+      e.style.display = "none";
       pa.style.display = "none";
       pd.style.display = "block";
       pd.style.display = "none";
       pd.style.display = "none";
+      pe.style.display = "none";
+    }
+  }
+
+  function ae() {
+    var a = document.getElementById("aa");
+    var b = document.getElementById("bb");
+    var c = document.getElementById("cc");
+    var d = document.getElementById("dd");
+    var e = document.getElementById("ee");
+    var pa = document.getElementById("point1");
+    var pb = document.getElementById("point2");
+    var pc = document.getElementById("point3");
+    var pd = document.getElementById("point4");
+    var pe = document.getElementById("point5");
+    if (e.style.display === "none") {
+      e.style.display = "block";
+      a.style.display = "none";
+      b.style.display = "none";
+      c.style.display = "none";
+      d.style.display = "none";
+      pa.style.display = "none";
+      pd.style.display = "block";
+      pb.style.display = "none";
+      pc.style.display = "none";
+      pe.style.display = "none";
+    } else {
+      e.style.display = "block";
+      a.style.display = "none";
+      b.style.display = "none";
+      c.style.display = "none";
+      d.style.display = "none";
+      pa.style.display = "none";
+      pd.style.display = "none";
+      pd.style.display = "none";
+      pd.style.display = "none";
+      pe.style.display = "block";
     }
   }
