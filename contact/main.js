8123dc5ad567fc83541d3a760b5f9319f58792bd
digitsmart <69549818+digitsmart@users.noreply.github.com>
2021-01-28 05:33:32 -0800

    Add files via upload

---
+++ contact/main.js
@@ -0,0 +1,59 @@
+// Initialize Firebase (ADD YOUR OWN DATA)
+var config = {
+  apiKey: "AIzaSyDVjzbTqXNzNsteN9FtUnLzG7J9G1h-Pkg",
+	authDomain: "signup-de54d.firebaseapp.com",
+	databaseURL: "https://signup-de54d.firebaseio.com",
+	projectId: "signup-de54d",
+	storageBucket: "signup-de54d.appspot.com",
+	messagingSenderId: "313335894880",
+};
+firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var company = getInputVal('company');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, company, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company:company,
+    email:email,
+    phone:phone,
+    message:message
+  });
+}
